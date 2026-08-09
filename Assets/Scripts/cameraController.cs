379493204e545f046d76f42759d0892b6813eb4f
ptaetoe <138846269+ptaetoe@users.noreply.github.com>
2026-08-09 20:35:54 +0400

    Moved cam position a bit more right to the player

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -16,6 +16,6 @@ public class cameraController : MonoBehaviour
     }
     private void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x + 3, player.transform.position.y, transform.position.z);
     }
 }
