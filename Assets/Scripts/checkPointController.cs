f25386e2dbfdad7149f589357cdcf20f63af8c7e
Sohil abuzeid <Sohil.abuzeid@gmail.com>
2026-08-27 06:59:49 +0300

    fix

--- Assets/Scripts/checkPointController.cs
+++ Assets/Scripts/checkPointController.cs
@@ -9,7 +9,8 @@ public class checkPointController : MonoBehaviour
 
     private Animator animator;
     private Vector2 spawnPoint;
-    private lightController lightController;
+    //private lightController lightController;
+    Light2D light2D;
     private playerController2 player;
     // Start is called before the first frame update
     void Start()
@@ -17,8 +18,11 @@ public class checkPointController : MonoBehaviour
         animator = GetComponent<Animator>();
         spawnPoint = transform.position;
         spawnPoint.y += verticalShift;
-        lightController = GetComponent<lightController>();
-        lightController.push(transform.Find("light1").GetComponent<Light2D>());
+        //lightController = GetComponent<lightController>();
+        //lightController.push(transform.Find("light1").GetComponent<Light2D>());
+        //lightController.turnOof();
+        light2D = transform.Find("light1").GetComponent<Light2D>();
+        light2D.intensity = 0f;
     }
 
     // Update is called once per frame
@@ -34,7 +38,8 @@ public class checkPointController : MonoBehaviour
                  player = collision.gameObject.GetComponent<playerController2>();
             if(collision!=player.circleCollider)
             {
-                lightController.turnON();
+                //lightController.turnON();
+                light2D.intensity = 2f;
                 animator.SetBool("flagged_bool", true);
                 player.resetSpawn(spawnPoint);
             }
